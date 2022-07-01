240bbf30cfe21d9542f6576f937980a19e6d3ff1
huskyhehe <zhou.he1@northeastern.edu>
2022-06-30 21:40:19 -0700

    Add core

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,3 @@
-from re import A
 from django.urls import path
 from .views import FileUploadView
 
@@ -8,15 +7,6 @@ urlpatterns = [
     path('demo/', FileUploadView.as_view(), name='file-upload-demo'),
 ]
 
-# urlpatterns = [
-#     path('demo/', FileUploadView.as_view(), name='file-upload-demo'),
-#     path('email/', UserEmailView.as_view(), name='email'),
-#     path('change-email/', ChangeEmailView.as_view(), name='change-email'),
-#     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
-#     path('billing/', UserDetailsView.as_view(), name='billing'),
-#     path('subscribe/', SubscribeView.as_view(), name='subscribe'),
-#     path('cancel-subscription/', CancelSubscription.as_view(),
-#          name='cancel-subscription'),
-#     path('upload/', ImageRecognitionView.as_view(), name='image-recognition'),
-#     path('api-key/', APIKeyView.as_view(), name='api-key')
-# ]
+urlpatterns = [
+    path('demo/', FileUploadView.as_view(), name='file-upload-demo')
+]
